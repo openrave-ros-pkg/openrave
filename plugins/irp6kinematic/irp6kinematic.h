548f76f525d819ad9cadc77188d909e8f662a73a
Przemo <Fokaszot@gmail.com>
2015-04-10 17:00:41 +0200

    Dodanie ik irp6 jako oddzielny plugin

---
+++ plugins/irp6kinematic/irp6kinematic.h
@@ -0,0 +1,272 @@
+// -*- coding: utf-8 -*-
+// Copyright (C) 2006-2011 Rosen Diankov <rosen.diankov@gmail.com>
+//
+// This file is part of OpenRAVE.
+// OpenRAVE is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#ifndef OPENRAVE_IRP6KINEMATIC
+#define OPENRAVE_IRP6KINEMATIC
+
+#include <openrave/planningutils.h>
+#include <cstdlib>
+
+
+#include <sstream>
+
+/// manages all connections. At the moment, server can handle only one connectino at a time
+class Irp6Kinematic : public ModuleBase
+{
+public:
+    Irp6Kinematic(EnvironmentBasePtr penv) : ModuleBase(penv)
+    {
+		__description=":Interface Author: Rosen Diankov\n\nSimple text-based server using sockets.";
+		RegisterCommand("ikSolvePost",boost::bind(&Irp6Kinematic::ikSolvePost, this,_1,_2),"solves ik for postument");
+        //RegisterCommand("fkSolvePost",boost::bind(&Irp6Kinematic::fkSolvePost, this,_1,_2),"solves fk for postument");
+    }
+
+    virtual ~Irp6Kinematic()
+    {
+        Destroy();
+    }
+
+    virtual int main(const std::string& cmd)
+    {
+        return 0;
+    }
+
+    virtual void Destroy()
+    {
+        Reset();
+    }
+
+    virtual void Reset()
+    {
+
+    }
+
+
+
+protected:
+	bool ikSolvePost(ostream& sout, istream& sinput)
+	{
+		//dane pobrane z ws_irp6/underylay/src/irp6_robot/irp6pm/src/Irp6pmKinematic.h
+		double a2 = 0.455;
+		double a3 = 0.67;
+		double d5 = 0.19;
+		double d6 = 0.25;
+		
+		double z_offset_const=0.95;
+		//Wczytywanie aktualnej pozycji robota z strumeinia wejsciowego
+		std::vector<double> local_current_joints(6);
+		sinput >> local_current_joints[0];
+		sinput >> local_current_joints[1];
+		sinput >> local_current_joints[2];
+		sinput >> local_current_joints[3];
+		sinput >> local_current_joints[4];
+		sinput >> local_current_joints[5];
+		
+		std::vector<double> local_current_joints_tmp_(6);
+		
+		std::vector<double> local_desired_joints(6);
+		
+	// poprawka w celu uwzglednienia konwencji DH
+		for(int i=0;i<6;i++)
+		{	
+			local_current_joints_tmp_[i] = local_current_joints[i];
+		}
+
+		//local_current_joints_tmp_[2] += local_current_joints_tmp_[1] + M_PI_2;
+		//local_current_joints_tmp_[3] += local_current_joints_tmp_[2];
+
+		// Stale
+		const double EPS = 1e-10;
+
+		// Zmienne pomocnicze.
+		double Nx, Ox, Ax, Px;
+		double Ny, Oy, Ay, Py;
+		double Nz, Oz, Az, Pz;
+		double s0, c0, s1, c1, s3, c3, s4, c4;
+		double E, F, K, ro, G, H;
+		double t5, t_ok;
+
+		// Wczytanie zadanej pozycji z strumienia wejsciowego
+		
+		//std::cout << q0 << "  " << q1 << "  " << q2 << "  " << q3 << "\n";
+		
+		sinput >>Nx;
+		sinput >>Ox;
+		sinput >>Ax;
+		sinput >>Ny;
+		sinput >>Oy;
+		sinput >>Ay;
+		sinput >>Nz;
+		sinput >>Oz;
+		sinput >>Az;
+		sinput >> Px;
+		sinput >> Py;
+		sinput >> Pz;
+		Pz -= z_offset_const;
+		Pz-= d6*Az;
+		Py-= d6*Ay;
+		Px-= d6*Ax;
+		
+		//std::cout << local_current_joints[0] << " " << local_current_joints[1] << " " << local_current_joints[2] << " " << local_current_joints[3] << " " << local_current_joints[4] << " " << local_current_joints[5] << "\n"; 
+		//std::cout << Nx << "  " << Ox << "  " << Ax << "  " << Px << "\n"; 
+		//std::cout << Ny << "  " << Oy << "  " << Ay << "  " << Py << "\n"; 
+		//std::cout << Nz << "  " << Oz << "  " << Az << "  " << Pz << "\n"; 
+
+		//  Wyliczenie Theta1.
+		(local_desired_joints)[0] = (atan2(Py, Px));
+		s0 = sin((double) (local_desired_joints)[0]);
+		c0 = cos((double) (local_desired_joints)[0]);
+
+		// Wyliczenie Theta5.
+		c4 = Ay * c0 - Ax * s0;
+		// Sprawdzenie bledow numerycznych.
+		if (fabs(c4 * c4 - 1) > EPS)
+		s4 = sqrt(1 - c4 * c4);
+		else
+		s4 = 0;
+
+		// Wyliczenie Theta4 i Theta6.
+		if (fabs(s4) < EPS) {
+		printf("Osobliwosc p\n");
+		// W przypadku osobliwosci katowi theta4 przypisywana wartosc poprzednia.
+		(local_desired_joints)[3] = local_current_joints_tmp_[3];
+		t5 = atan2(c0 * Nx + s0 * Ny, c0 * Ox + s0 * Oy);
+
+		// Sprawdzenie warunkow.
+		t_ok = t5 + (local_desired_joints)[3];
+		if (fabs((double)(t_ok - local_current_joints_tmp_[5])) > fabs((double)(t5 - M_PI + (local_desired_joints)[3]- (local_current_joints_tmp_[5]))))
+		  t_ok = t5 - M_PI + (local_desired_joints)[3];
+		if (fabs((double)(t_ok - local_current_joints_tmp_[5]))
+			> fabs((double)(
+				t5 + M_PI + (local_desired_joints)[3]
+					- (local_current_joints_tmp_[5]))))
+		  t_ok = t5 + M_PI + (local_desired_joints)[3];
+
+		if (fabs((double)(t_ok - local_current_joints_tmp_[5]))
+			> fabs((double)(
+				t5 - 2 * M_PI + (local_desired_joints)[3]
+					- (local_current_joints_tmp_[5]))))
+		  t_ok = t5 - 2 * M_PI + (local_desired_joints)[3];
+		if (fabs((double)(t_ok - local_current_joints_tmp_[5]))
+			> fabs((double)(
+				t5 + 2 * M_PI + (local_desired_joints)[3]
+					- (local_current_joints_tmp_[5]))))
+		  t_ok = t5 + 2 * M_PI + (local_desired_joints)[3];
+
+		if (fabs((double)(t_ok - local_current_joints_tmp_[5]))
+			> fabs((double)(
+				t5 - (local_desired_joints)[3] - (local_current_joints_tmp_[5]))))
+		  t_ok = t5 - (local_desired_joints)[3];
+		if (fabs((double)(t_ok - local_current_joints_tmp_[5]))
+			> fabs((double)(
+				t5 - M_PI - (local_desired_joints)[3]
+					- (local_current_joints_tmp_[5]))))
+		  t_ok = t5 - M_PI - (local_desired_joints)[3];
+		if (fabs((double)(t_ok - local_current_joints_tmp_[5]))
+			> fabs((double)(
+				t5 + M_PI - (local_desired_joints)[3]
+					- (local_current_joints_tmp_[5]))))
+		  t_ok = t5 + M_PI - (local_desired_joints)[3];
+
+		if (fabs((double)(t_ok - local_current_joints_tmp_[5]))
+			> fabs((double)(
+				t5 - 2 * M_PI - (local_desired_joints)[3]
+					- (local_current_joints_tmp_[5]))))
+		  t_ok = t5 - 2 * M_PI - (local_desired_joints)[3];
+		if (fabs((double)(t_ok - local_current_joints_tmp_[5]))
+			> fabs((double)(
+				t5 + 2 * M_PI - (local_desired_joints)[3]
+					- (local_current_joints_tmp_[5]))))
+		  t_ok = t5 + 2 * M_PI - (local_desired_joints)[3];
+
+		(local_desired_joints)[5] = t_ok;
+		}
+		else 
+		{
+			t5 = atan2(-s0 * Ox + c0 * Oy, s0 * Nx - c0 * Ny);
+			t_ok = t5;
+
+			// Sprawdzenie warunkow.
+			if (fabs((double)(t_ok )) > fabs((double)(t5 - M_PI ))) t_ok = t5 - M_PI;
+			if (fabs((double)(t_ok )) > fabs((double)(t5 + M_PI ))) t_ok = t5 + M_PI;
+
+			(local_desired_joints)[5] = t_ok;
+			t_ok = atan2(c0 * Ax + s0 * Ay, Az);
+
+			if (fabs((double)(t_ok ))> fabs((double)(t_ok - M_PI )))t_ok = t_ok - M_PI;
+			if (fabs((double)(t_ok )) > fabs((double)(t_ok + M_PI )))
+			  t_ok = t_ok + M_PI;
+			(local_desired_joints)[3] = t_ok;
+		}  //: else
+
+		// Wyliczenie Theta2.
+		c3 = cos((double)(local_desired_joints)[3]);
+		s3 = sin((double)(local_desired_joints)[3]);
+
+		if(fabs(c3) > EPS) s4=Az/c3;
+		else s4=(c1*Ax+s1*Ay)/s3;
+
+		(local_desired_joints)[4] = atan2(s4, c4);
+
+		E = c0 * Px + s0 * Py - c3 * d5;
+		F = -Pz - s3 * d5;
+		G = 2 * E * a2;
+		H = 2 * F * a2;
+		K = E * E + F * F + a2 * a2 - a3 * a3;
+		ro = sqrt(G * G + H * H);
+
+		(local_desired_joints)[1] = atan2(K / ro, sqrt(1 - ((K * K) / (ro * ro))))
+		  - atan2(G, H);
+
+		// Wyliczenie Theta3.
+		s1 = sin((double)(local_desired_joints)[1]);
+		c1 = cos((double)(local_desired_joints)[1]);
+		(local_desired_joints)[2] = atan2(F - a2 * s1, E - a2 * c1);
+
+		// poprawka w celu dostosowania do konwencji DH
+		(local_desired_joints)[2] -= (local_desired_joints)[1] + M_PI_2;
+		(local_desired_joints)[3] -= (local_desired_joints)[2]
+		  + (local_desired_joints)[1] + M_PI_2;
+	
+	if(local_desired_joints[4]<-0.5) local_desired_joints[4]+=2*M_PI;
+	
+	//std::cout << local_desired_joints[0] << " " << local_desired_joints[1] << " " << local_desired_joints[2] << " " << local_desired_joints[3] << " " << local_desired_joints[4] << " " << local_desired_joints[5] << "\n"; 
+	//if(local_desired_joints[4] < 0 ) local_desired_joints[4] = -local_desired_joints[4]; 
+	sout << local_desired_joints[0];
+	sout << " ";
+	sout << local_desired_joints[1];
+	sout << " ";
+	sout << local_desired_joints[2];
+	sout << " ";
+	sout << local_desired_joints[3];
+	sout << " ";
+	sout << local_desired_joints[4];
+	sout << " ";
+	sout << local_desired_joints[5];
+	return true;	
+	}
+
+};
+
+#ifdef RAVE_REGISTER_BOOST
+#include BOOST_TYPEOF_INCREMENT_REGISTRATION_GROUP()
+
+BOOST_TYPEOF_REGISTER_TYPE(SimpleTextServer::Socket)
+BOOST_TYPEOF_REGISTER_TYPE(SimpleTextServer::WORKERSTRUCT)
+
+#endif
+
+#endif
